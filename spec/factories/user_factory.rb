c0dd826069e454cad07ace7151d243771c56c4b5
anakin78 <marcio.tedesco@gmail.com>
2014-09-07 15:06:03 -0300

    Implemented signing in.

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
 	factory :user do
 		name "Example project"
 		email "marcio.tedesco@gmail.com"
-		password_digest	"optimusprime"
+		password "optimusprime"
+		password_confirmation "optimusprime"
 	end
 end
