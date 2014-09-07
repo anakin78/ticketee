bd5bb2239522f21ee7bac25ca240674f98f4a9cf
anakin78 <marcio.tedesco@gmail.com>
2014-09-07 18:38:26 -0300

    Associate users to tickets when tickets are created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,5 +18,4 @@ class ApplicationController < ActionController::Base
 		end
 		
 		helper_method :current_user
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
 
   	def create
 		@user = User.new(user_params)
-		
 		if @user.save
 			flash[:notice] = "You have signed up successfully."
 			redirect_to projects_path
@@ -39,7 +38,7 @@ class UsersController < ApplicationController
   	end
 
 	def user_params
-		params.require(:user).permit(:name,:password, :password_confirmation)
+		params.require(:user).permit(:name,:email,:password, :password_confirmation)
 	end
 	
 end
