50363eef285c2311adcb480ed8f952b8fd8a303e
anakin78 <marcio.tedesco@gmail.com>
2014-09-08 19:11:23 -0300

    adicionado project_params no projectcontroller da api

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -4,12 +4,18 @@ class Api::V1::ProjectsController < Api::V1::BaseController
 	end
 
 	def create
-		project = Project.new(params[:project])
+		project = Project.new(project_params)
 		if project.save
 			respond_with(project, :location => api_v1_project_path(project))
 		else
-		respond_with(project)
+			respond_with(project)
 		end
+	end
+
+	private
+
+	def project_params
+			params.require(:project).permit(:name, :description)
 	end
 
 end
