3b4346987dc684c0611548cc768b4987fa134055
anakin78 <marcio.tedesco@gmail.com>
2014-09-15 22:14:32 -0300

    Removido comentarios do api_helper_spec.rb e criado um arquivo projects_spec.rb simples que esta funcionando

--- spec/api/v1/projects_spec.rb
+++ spec/api/v1/projects_spec.rb
@@ -1,10 +1,5 @@
-#require "spec_helper"
+require "spec_helper"
 
-#describe "/api/v1/projects", :type => :api do <co id="ch13_20_1"/>
-
-#describe "/api/v1/projects", :type => :api do
-#	let!(:admin) { Factory(:admin_user) }
-#end
-
-
-#end
+describe "/api/v1/projects", type: :api do
+	let!(:user) { FactoryGirl.create(:user) }
+end
--- spec/support/api_helpers.rb
+++ spec/support/api_helpers.rb
@@ -1,11 +1,11 @@
-#module ApiHelper
+module ApiHelper
 
-#include Rack::Test::Methods <co id="ch13_458_1"/>
-#	def app <co id="ch13_458_2"/>
-#		Rails.application
-#	end
-#end
+include Rack::Test::Methods
+	def app
+		Rails.application
+	end
+end
 
-#RSpec.configure do |c|
-	#c.include ApiHelper, :type => :api <co id="ch13_458_3"/>
-#end
+RSpec.configure do |c|
+	c.include ApiHelper, type: :api 
+end
